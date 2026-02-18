13f0acd577965a3542bb551197dbd54c3cf71a82
John Hamwi <john.hamwi10@gmail.com>
2026-02-18 07:23:41 -0500

    Move remaining standalone services to folder structure

---
+++ src/services/presence/__init__.py
@@ -0,0 +1,5 @@
+"""SyriaBot - Presence Service Package."""
+
+from src.services.presence.service import PresenceHandler, BasePresenceHandler
+
+__all__ = ["PresenceHandler", "BasePresenceHandler"]
---
+++ src/services/sync_profile/__init__.py
@@ -0,0 +1,5 @@
+"""SyriaBot - Sync Profile Service Package."""
+
+from src.services.sync_profile.service import ProfileSyncService
+
+__all__ = ["ProfileSyncService"]
